bb542f801d43869a60e8ea03dbe8dec2cf12f074
N1ceCode <g310lj@qq.com>
2024-11-05 17:23:39 +0800

    add template

---
+++ 18.0/Brick stacks/sol1.py
@@ -0,0 +1,12 @@
+
+def solve():
+    pass
+
+def main():
+    N, x = map(int, input().split())
+    A_list = list(map(int, input().split()))
+    print(N,x,A_list)
+    solve()
+      
+if __name__ == '__main__':
+    main()
